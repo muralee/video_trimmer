ec011e074f07274f064b54bf5e69cd40dd741415
murali <muralee14@gmail.com>
2022-06-08 21:45:15 +0400

    sample updated

--- example/lib/trimmer_view.dart
+++ example/lib/trimmer_view.dart
@@ -28,8 +28,14 @@ class _TrimmerViewState extends State<TrimmerView> {
     _loadVideo();
   }
 
-  void _loadVideo() {
-    _trimmer.loadVideo(videoFile: widget.file);
+  void _loadVideo() async {
+    debugPrint('---TrimVideo: Source Video Size in bytes: ' +
+        widget.file.lengthSync().toString());
+    await _trimmer.loadVideo(videoFile: widget.file);
+    if(_trimmer.videoPlayerController != null) {
+      debugPrint('---TrimVideo: video duration: ' +
+          _trimmer.videoPlayerController!.value.duration.inSeconds.toString());
+    }
   }
 
   _saveVideo() {
@@ -37,14 +43,36 @@ class _TrimmerViewState extends State<TrimmerView> {
       _progressVisibility = true;
     });
 
+    //String ffmpegCommand = ' -c:v libx264 -preset fast -crf 18 -pix_fmt yuv420p -c:a copy ';
+    String ffmpegCommand = ' -c:v libx264 -crf 24 -pix_fmt yuv420p -c:a copy ';
+    //String ffmpegCommand = ' -c:v libx265 -crf 28 -pix_fmt yuv420p -c:a copy ';
+    //String ffmpegCommand = ' -c:v libx264 -pix_fmt yuv420p -c:a copy ';
+    //String ffmpegCommand = ' -c:v copy -pix_fmt yuv420p -c:a copy ';
+    FileFormat outputFormat = FileFormat.mp4;
+    String customVideoFormat= outputFormat.toString();
+
     _trimmer.saveTrimmedVideo(
       startValue: _startValue,
       endValue: _endValue,
+
+      // advanced start
+      ffmpegCommand: ffmpegCommand,
+      customVideoFormat: customVideoFormat,
+      outputFormat: outputFormat,
+      // advanced end
+
       onSave: (outputPath) {
         setState(() {
           _progressVisibility = false;
         });
         debugPrint('OUTPUT PATH: $outputPath');
+
+        if (null != outputPath) {
+          File _file = File(outputPath);
+          debugPrint('---TrimVideo: out video size in bytes: ' +
+              _file.lengthSync().toString());
+        }
+
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
             builder: (context) => Preview(outputPath),
